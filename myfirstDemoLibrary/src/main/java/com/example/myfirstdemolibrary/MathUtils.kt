5e1ff2b8f35395b9820c0913d1d367ef9ca1186b
KTPLC-10131 <dnyanesh.kalal@kpoint.com-at-701980022429>
2023-11-16 10:48:33 +0530

    First Commits

---
+++ myfirstDemoLibrary/src/main/java/com/example/myfirstdemolibrary/MathUtils.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstdemolibrary
+
+class MathUtils {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+}
