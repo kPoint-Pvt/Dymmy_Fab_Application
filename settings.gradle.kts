98d4df190021a7497c65ddbe9269828f31d59125
KTPLC-10131 <dnyanesh.kalal@kpoint.com-at-701980022429>
2023-11-16 11:02:01 +0530

    First Commits

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dymmy Fab Application"
+include(":app")
+include(":myfirstDemoLibrary")
+include(":myfablibrary")
